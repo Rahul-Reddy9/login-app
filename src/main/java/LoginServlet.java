3ce77666bc839369f09d84728d18a4c83e10cb41
Geggalapally Rahul Reddy <grahulreddy38@gmail.com>
2026-05-02 11:49:10 +0530

    changing loginservelet code

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,8 +1,3 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
@@ -12,10 +7,26 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body style='font-family:Arial;'>");
+        out.println("<h2 style='text-align:center;'>Login Result</h2>");
+
+        out.println("<table border='1' align='center' cellpadding='10'>");
+
         if ("admin".equals(username) && "admin123".equals(password)) {
-            response.getWriter().println("Login Successful");
+
+            out.println("<tr><th>Status</th><td style='color:green;'>Login Successful</td></tr>");
+            out.println("<tr><th>Username</th><td>" + username + "</td></tr>");
+
         } else {
-            response.getWriter().println("Login Failed");
+
+            out.println("<tr><th>Status</th><td style='color:red;'>Invalid Credentials</td></tr>");
+            out.println("<tr><th>Username</th><td>" + username + "</td></tr>");
         }
+
+        out.println("</table>");
+        out.println("</body></html>");
     }
 }
