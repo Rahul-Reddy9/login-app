d1fdd04fa0f5861f80bf5991a573f854cf3b5965
Geggalapally Rahul Reddy <grahulreddy38@gmail.com>
2026-05-02 11:20:21 +0530

    adding complete forms

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            response.getWriter().println("Login Successful");
+        } else {
+            response.getWriter().println("Login Failed");
+        }
+    }
+}
